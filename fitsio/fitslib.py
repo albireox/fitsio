9e1c0a2ed297bd008a7a2d1a2ec80fcfa734ff5d
Erin Sheldon <erin.sheldon@gmail.com>
2018-03-21 09:57:15 -0400

    Reverting to the state of the project before ignore empty merge

--- fitsio/fitslib.py
+++ fitsio/fitslib.py
@@ -420,7 +420,6 @@ class FITS(object):
               compress=None, tile_dims=None,
               header=None,
               names=None,
-              ignore_empty=False,
               table_type='binary', write_bitcols=False, **keys):
         """
         Write the data to a new HDU.
@@ -482,7 +481,6 @@ class FITS(object):
 
         if isimage:
             self.write_image(data, extname=extname, extver=extver, 
-                             ignore_empty=ignore_empty,
                              compress=compress, tile_dims=tile_dims,
                              header=header)
         else:
@@ -494,7 +492,7 @@ class FITS(object):
 
 
 
-    def write_image(self, img, extname=None, extver=None, ignore_empty=False,
+    def write_image(self, img, extname=None, extver=None,
                     compress=None, tile_dims=None, header=None):
         """
         Create a new image extension and write the data.  
@@ -536,7 +534,7 @@ class FITS(object):
         """
 
         self.create_image_hdu(img, 
-                              header=header, ignore_empty=ignore_empty,
+                              header=header,
                               extname=extname, extver=extver,
                               compress=compress, tile_dims=tile_dims)
 
@@ -556,8 +554,7 @@ class FITS(object):
                          extver=None,
                          compress=None,
                          tile_dims=None,
-                         header=None,
-                         ignore_empty=False):
+                         header=None):
         """
         Create a new, empty image HDU and reload the hdu list.  Either
         create from an input image or from input dims and dtype
@@ -641,8 +638,7 @@ class FITS(object):
                     img2send = array_to_native(img, inplace=False)
 
             else:
-                if not ignore_empty:
-                    self._ensure_empty_image_ok()
+                self._ensure_empty_image_ok()
                 compress=None
                 tile_dims=None
 
